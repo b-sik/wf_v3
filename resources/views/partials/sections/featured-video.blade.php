44f45d1b59a6f63bdb425c1c8019977467110ba3
Brian Siklinski <bszyk.codes@gmail.com>
2022-08-30 14:32:50 -0400

    use yarn & update video section

--- resources/views/partials/sections/featured-video.blade.php
+++ resources/views/partials/sections/featured-video.blade.php
@@ -5,10 +5,11 @@ $video_desc = get_field('featured_video_desc', $home->ID);
 @endphp
 
 <section id="featured_video" class="container d-flex flex-column align-items-center">
-    <div class="card">
-        <div class="embed-container">
-            {!! $video !!}
+    <div class="row">
+        <div class="col-12">
+            <div class="embed-container">
+                {!! $video !!}
+            </div>
         </div>
-        <small class="text-center card-text d-block my-3"><em class="text-dark">{!! $video_desc !!}</em></small>
     </div>
 </section>
